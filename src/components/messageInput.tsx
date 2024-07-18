059408965a39c7958894c820e48399f1f94b1154
Soichi Ikebe <souichi1997@gmail.com>
2024-07-19 01:58:49 +0900

    クレジット表記の修正

--- src/components/messageInput.tsx
+++ src/components/messageInput.tsx
@@ -49,7 +49,7 @@ export const MessageInput = ({
           </div>
         </div>
         <div className="py-4 bg-[#413D43] text-center text-white font-Montserrat">
-          powered by VRoid, Koemotion, ChatGPT API
+          Powered by GPT-3.5, VOICEVOX: 四国めたん
         </div>
       </div>
     </div>
